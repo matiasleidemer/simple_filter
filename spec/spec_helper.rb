85f495a844c773134696b53dbcbc22335e189cba
Matias Leidemer <matiasleidemer@gmail.com>
2015-05-05 16:22:56 -0300

    Update spec_helper to include some data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,11 @@ require 'simple_filter'
 require File.dirname(__FILE__) + '/support/connection'
 
 class Product < ActiveRecord::Base
+  scope :active, -> { where active: true }
+  scope :with_name, -> (name) { where 'products.name like ?', "%#{name}%" }
 end
 
-class ProductFilter < SimpleFilter::Base
-end
+Product.create!(name: 'Book', price: 2.99, start_at: DateTime.now, end_at: DateTime.now + 7, active: true)
+Product.create!(name: 'Pen', price: 26.49, start_at: DateTime.now, end_at: DateTime.now + 6, active: false)
+Product.create!(name: 'Notebook', price: 48.99, start_at: DateTime.now, end_at: DateTime.now + 5, active: true)
+Product.create!(name: 'Mouse', price: 51.49, start_at: DateTime.now, end_at: DateTime.now + 4, active: true)
--- spec/support/connection.rb
+++ spec/support/connection.rb
@@ -1,5 +1,6 @@
 require 'active_record'
 
 ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+ActiveRecord::Migration.verbose = false
 
 load File.dirname(__FILE__) + '/schema.rb'
